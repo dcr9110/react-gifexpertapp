b1d2416202f9652cfba133403a21918dd176a667
Dcano <diegocn217@gmail.com>
2020-08-14 02:18:08 -0300

    renombrar la carpeta build

---
+++ docs/precache-manifest.e36353a42593bd9596393a384a266203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc55e4d3c069a53a78f8ee3b8593bcb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b4a0f36036b6d5a9393",
+    "url": "/static/css/main.4c42159a.chunk.css"
+  },
+  {
+    "revision": "962f49d9317be141c683",
+    "url": "/static/js/2.1c4e3a24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1c4e3a24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4a0f36036b6d5a9393",
+    "url": "/static/js/main.42923832.chunk.js"
+  },
+  {
+    "revision": "3f809adccd7b5eb81ce7",
+    "url": "/static/js/runtime-main.6d8ceafa.js"
+  }
+]);
