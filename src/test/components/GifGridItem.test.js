42b88d676b11452ca8b66546412994c5d789fc59
Dcano <diegocn217@gmail.com>
2020-09-13 23:02:42 -0300

    completado el ejercicio practico de GifGridItem.test

--- src/test/components/GifGridItem.test.js
+++ src/test/components/GifGridItem.test.js
@@ -20,13 +20,28 @@ describe('Pruebas en <GifGridItem/>', ()=>{
     test('debe tener un parrafo con el title', () => {
         
         const p = wrapper.find('p');
+        
         expect( p.text().trim() ).toBe( title ); 
 
     })
     
 
-    test('should ', () => {
+    test('debe tener la imagen igual al url y alt de los props ', () => {
         
+        const img = wrapper.find('img');
+        
+        expect( img.props().src ).toBe( url );
+        expect( img.props().alt ).toBe( title );
+        
+    })
+    
+    test('debe tener la clase animate__jello', () => {
+        
+        const div = wrapper.find('div');
+        const className = div.prop('className');
+
+        expect( className.includes( 'animate__jello' ) ).toBe( true );
+
     })
     
 })
