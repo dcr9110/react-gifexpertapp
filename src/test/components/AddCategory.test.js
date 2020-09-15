15333e463269ec1c8333902c1a29814785a28f5c
Dcano <diegocn217@gmail.com>
2020-09-15 02:31:41 -0300

    se realizo nuevas pruebas al componente AddCategory ej10

--- src/test/components/AddCategory.test.js
+++ src/test/components/AddCategory.test.js
@@ -31,7 +31,28 @@ describe('Pruebas con <AddCategory/>', () =>{
         expect( setCategory ).not.toHaveBeenCalled();
     })
     
+    test('debe de llamar el setCategories y limpiar la caja de texto', () => {
+        const input = wrapper.find('input');
+        const value = 'hola mundo';
 
+        //1. simular el inputChange
+        input.simulate('change', { target: { value } });
+ 
+        //2. simular el submit
+        wrapper.find('form').simulate('submit', { preventDefault() { } });
+
+        //3. setCategories se debe de haber llamado
+        expect( setCategory ).toHaveBeenCalled();
+        // se llamo una vez
+        expect( setCategory ).toHaveBeenCalledTimes(1);
+        //se llamo una funcion
+        expect( setCategory ).toHaveBeenCalledWith( expect.any(Function) );
+
+        //4. el valor del input debe de ser ''
+        expect( input.props().value ).toBe('');
+
+    })
+    
     
     
 })
